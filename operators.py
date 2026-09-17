74982d26600ea0d5899503a80791356cdf18d0ee
Chethan Yadav <chethanyadav456@gmail.com>
2026-09-17 16:18:54 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,41 @@
+# Python Operators
+
+# +, -, *, /, %, //, **
+a = 10
+b = 5
+
+# print(a + b) # Output: 15
+# print(a ** 2)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# =,
+temp_var = "test"
+# a = 10
+# a = a + 10
+a += 10 # Output : 20
+a -= 20 # Output: 0
+
+# ==, !=, >, <, >=, <=
+
+# x = int(input("Enter the value for var x: "))
+# y = int(input("Enter the value for var y: "))
+
+# print(x == y)
+# print(x != y) 
+# print(x > y) 
+# print(x < y) 
+# print(x >= y)
+# print(x <= y) # 10000 <= 5000: False
+
+# Logical Ops
+# and or not
+
+print(10 == 10 and 10 > 20) # False
+print(10 == 20 or 30 > 40) #
+print(not(10 == 20 or 30 > 40)) #
+
+# nums, chars, symbols
